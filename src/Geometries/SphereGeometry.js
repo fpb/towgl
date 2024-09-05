815b3b264c16fc130d3fa95e58b41980faff99e2
Fernando Birra <fpb@fct.unl.pt>
2024-09-05 11:11:12 +0000

    Added more code to SphereGeometry skeleton

--- src/Geometries/SphereGeometry.js
+++ src/Geometries/SphereGeometry.js
@@ -8,13 +8,39 @@ import { Vector3 } from '../math/Vector3.js';
  */
 
 export class SphereGeometry extends BufferedGeometry {
-    constructor() {
+    constructor( radius = 1, meridians = 32, parallels = 16, startAngle = 0, apertureAngle = Math.PI * 2, thetaStart = 0, thetaLength = Math.PI )
+    {
         super();
 
         const indices = [];
         const vertices = [];
         const normals = [];
         const uvs = [];
+
+        const deltaAngle = apertureAngle / meridians;
+        const deltaParallels = Math.PI / (2 * parallels);
+
+        // Add top of sphere
+        vertices.push(0, 1, 0);
+        normals.push(0, 1, 0);
+        uvs.push(0.5, 1);
+
+        // Add top cap
+
+        // Add top part
+
+
+        // Add bottom of sphere
+        vertices.push(0, -1, 0);
+        normals.push(0, -1, 0);
+        uvs.push(0.5, 0);
+
+        // Add bottom cap
+
+        // Add bottom part
+
+        // Add the indices
+
 
         this.setIndices(indices);
         this.setAttribute('position', new Float32BufferedAttribute(vertices, 3, false));
